3345395126dfe2ed2e034b8940cc02dd1b1dbfd7
Gary Kirrene <gkirrene@gmail.com>
2018-07-21 19:14:05 -0700

    added test.js difference test

--- test/test.js
+++ test/test.js
@@ -6,6 +6,11 @@ test('Should return 4', function(t) {
     t.end();
 });
 
+test('Should return 2', function(t) {
+    t.equal(2, work.difference(4,2));
+    t.end();
+});
+
 test('Should return Gary', function(t) {
     t.equal('Gary', work.blah);
     t.end();
